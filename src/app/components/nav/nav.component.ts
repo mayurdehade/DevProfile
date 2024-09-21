6086f1547afe6ee7d05474136b4d42ba3ebb5233
Mayur <mayurdehade0504@gmail.com>
2024-09-21 18:21:25 +0530

    project completed

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -10,24 +10,14 @@ export class NavComponent {
 
   darkModeService: DarkmodeService = inject(DarkmodeService)
 
+  theme: string = 'null';
+
+
   toggleDarkMode():void {
-    this.darkModeService.updateDarkMode()
+    this.darkModeService.updateDarkMode();
+    this.theme = this.darkModeService.darkModeSignal();
   }
 
-//   var toggleOpen = document.getElementById('toggleOpen');
-// var toggleClose = document.getElementById('toggleClose');
-// var collapseMenu = document.getElementById('collapseMenu');
-
-// function handleClick() {
-//   if (collapseMenu.style.display === 'block') {
-//     collapseMenu.style.display = 'none';
-//   } else {
-//     collapseMenu.style.display = 'block';
-//   }
-// }
-
-// toggleOpen.addEventListener('click', handleClick);
-// toggleClose.addEventListener('click', handleClick);
 
   collapseMenu:any;
 
